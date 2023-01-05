6c7e7922a0d32ae1014d78b4bd88ca70cd832e31
Marco Perone <marco.perone@tweag.io>
2023-01-05 11:09:07 +0100

    define Topology for a state machine

--- src/BaseMachine.hs
+++ src/BaseMachine.hs
@@ -1,4 +1,19 @@
-module BaseMachine where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
-foo :: Int
-foo = "adsfas" :: Int
+module BaseMachine () where
+
+import "base" Data.Kind (Type)
+import "singletons-base" Data.Singletons.Base.TH (singletons)
+
+{- | A `Topology` is a description of the topology of a state machine
+   It contains the collection of allowed transitions
+-}
+$( singletons
+    [d|
+      newtype Topology (vertex :: Type) = MkTopology {edges :: [(vertex, [vertex])]}
+      |]
+ )
