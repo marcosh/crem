b9a99914cc4cb3b0b1f86313a6140e50dfbadff0
Marco Perone <marco.perone@tweag.io>
2023-01-11 16:22:51 +0100

    run doctests as part of cabal test

---
+++ doctest/Main.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import "base" System.Environment (getArgs)
+import "doctest-parallel" Test.DocTest (mainFromCabal)
+
+main :: IO ()
+main = do
+  mainFromCabal "crm" =<< getArgs
