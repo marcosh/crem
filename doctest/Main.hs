1dcce3a6733a23ee8b0c7c81fe5c7f7a6deae9c2
Marco Perone <marco.perone@tweag.io>
2023-02-27 17:03:48 +0100

    execute doctests also on examples

--- doctest/Main.hs
+++ doctest/Main.hs
@@ -1,8 +1,15 @@
 module Main where
 
 import "base" System.Environment (getArgs)
-import "doctest-parallel" Test.DocTest (mainFromCabal)
+import "doctest-parallel" Test.DocTest
+import "doctest-parallel" Test.DocTest.Helpers
 
 main :: IO ()
 main = do
-  mainFromCabal "crem" =<< getArgs
+  args <- getArgs
+  cremPackage <- findCabalPackage "crem"
+  cremLib <- extractCabalLibrary cremPackage
+  cremExamplesLib <- extractSpecificCabalLibrary (Just "crem-examples") cremPackage
+  let
+    wholeCremLib = mergeLibraries [cremLib, cremExamplesLib]
+  mainFromLibrary wholeCremLib args
