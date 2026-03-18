4a9a1ee353e6790267c10c96d5372efc522e9d1a
harryprayiv <harryprayiv@gmail.com>
2026-03-18 11:13:16 -0400

    Fix doctests for GHC 9.10.3 by upgrading to cabal-version 3.0, using qualified internal library syntax, and rewriting the doctest runner to inject the crem-examples package-id directly into evalGhciArgs via runModules.

--- doctest/Main.hs
+++ doctest/Main.hs
@@ -1,16 +1,44 @@
+{-# LANGUAGE ImplicitParams #-}
+
 module Main where
 
-import "base" System.Environment (getArgs)
-import "doctest-parallel" Test.DocTest
+import "base" Control.Monad (unless)
+import "base" Data.List (isPrefixOf, isInfixOf)
+import "base" System.Exit (exitFailure)
+import "directory" System.Directory (getCurrentDirectory, listDirectory)
+import "filepath" System.FilePath ((</>))
+import "doctest-parallel" Test.DocTest (isSuccess, setSeed)
 import "doctest-parallel" Test.DocTest.Helpers
+import "doctest-parallel" Test.DocTest.Internal.Logging (LogLevel (..))
+import "doctest-parallel" Test.DocTest.Internal.Options (defaultModuleConfig)
+import "doctest-parallel" Test.DocTest.Internal.Runner (runModules)
 
 main :: IO ()
 main = do
-  args <- getArgs
   cremPackage <- findCabalPackage "crem"
   cremLib <- extractCabalLibrary cremPackage
   cremExamplesLib <- extractSpecificCabalLibrary (Just "crem-examples") cremPackage
+  let wholeCremLib = mergeLibraries [cremLib, cremExamplesLib]
+  pkgId <- findCremExamplesId
   let
-    base = mergeLibraries [cremLib, cremExamplesLib]
-    wholeCremLib = base
-      { <ghcOptionsField> = "-package-id" : "crem-examples" : <ghcOptionsField> base }
+    (includeArgs, allModules, otherGhciArgs) = libraryToGhciArgs wholeCremLib
+    evalGhciArgs = otherGhciArgs ++ ["-package-id", pkgId]
+    parseGhcArgs = includeArgs ++ otherGhciArgs
+  modConfig <- let ?verbosity = Info in setSeed defaultModuleConfig
+  summary <- let ?verbosity = Info in runModules modConfig Nothing True parseGhcArgs evalGhciArgs allModules
+  unless (isSuccess summary) exitFailure
+
+findCremExamplesId :: IO String
+findCremExamplesId = do
+  dir <- getCurrentDirectory
+  files <- listDirectory dir
+  let envFiles = filter (isPrefixOf ".ghc.environment.") files
+  case envFiles of
+    [] -> error "No .ghc.environment file found; run cabal build first"
+    (f:_) -> do
+      contents <- readFile (dir </> f)
+      let pkgIdLines = filter (isPrefixOf "package-id") (lines contents)
+          examplesLines = filter (isInfixOf "crem-examples") pkgIdLines
+      case examplesLines of
+        [] -> error "Could not find crem-examples package-id in .ghc.environment file"
+        (l:_) -> pure (drop (length "package-id ") l)
